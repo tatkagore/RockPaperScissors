3329a0ade4ea5ddafc0054341348a92c4b7e9718
Tatiana Simmer <tatkagore@gmail.com>
2022-05-20 17:36:30 +0200

    Initial Commit

---
+++ Shared/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
